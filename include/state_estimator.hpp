16062a019aa6093a19e83667af08064ad5c9d0f7
DS3a <33351122+DS3a@users.noreply.github.com>
2026-01-17 03:12:08 +0100

    state estimator works

--- include/state_estimator.hpp
+++ include/state_estimator.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+
+// #define CHECK_RMSE
 #include <Eigen/Core>
 
 #include <pinocchio/parsers/sdf.hpp>
@@ -9,6 +11,8 @@
 #include <pinocchio/algorithm/contact-info.hpp>  // for RigidConstraintModel (depending on version)
 
 #include <inverse_kinematics.hpp>
+
+
 
 #include <cmath>
 #include <iostream>
@@ -29,11 +33,15 @@ class StateEstimator {
         joint_positions_(pin_model_->nq),
         joint_velocities_(pin_model_->nv) {
         
+
+    std::cout << "received pinocchio model with nq " << pin_model_->nq 
+              << " and nv " << pin_model_->nv << "\n";
     this->frame_ids_ = frame_ids;
     this->pin_data_ = std::make_shared<pinocchio::Data>(*pin_model_);
-    this->joint_positions_.setZero();
+    // this->joint_positions_.setZero();
     this->joint_velocities_.setZero();
 
+    this->joint_positions_ = pinocchio::neutral(*(this->pin_model_));
     int num_links = this->pin_model_->names.size();
     this->num_elements_x_ = static_cast<int>(std::sqrt(num_links));
     this->num_elements_y_ = this->num_elements_x_;
@@ -54,25 +62,12 @@ class StateEstimator {
 
   void updateEstimations() {
 
-    Eigen::VectorXd vel_updates = Eigen::VectorXd::Zero(this->pin_model_->nv);
     Eigen::VectorXd prev_pos = this->joint_positions_;
+
+    #ifdef CHECK_RMSE
     double prev_rmse = std::numeric_limits<double>::infinity();
 
-    for (int iter=0; iter<2; ++iter) {
-      Eigen::VectorXd dq = skyweave::SolveIKStep(
-          *(this->pin_model_), 
-          *(this->pin_data_),
-          this->joint_positions_,
-          /* TODO add constraints=*/this->constraints,
-          this->position_measurements,
-          this->frame_ids_);
-
-      Eigen::VectorXd next_pos = Eigen::VectorXd::Zero(this->pin_model_->nq);
-      pinocchio::integrate(
-          *(this->pin_model_), this->joint_positions_, dq, next_pos);
-        
-      this->joint_positions_ = next_pos;
-      
+      this->joint_positions_ = pinocchio::neutral(*(this->pin_model_));
       // TODO check the link positions and compare them to the current model state
       pinocchio::forwardKinematics(*(this->pin_model_), *(this->pin_data_), this->joint_positions_);
       pinocchio::updateFramePlacements(*(this->pin_model_), *(this->pin_data_));
@@ -97,11 +92,63 @@ class StateEstimator {
       const double rmse = sample_count > 0
                               ? std::sqrt(sum_squared_error / static_cast<double>(sample_count))
                               : 0.0;
+
+      prev_rmse = rmse;
+
+      std::cout << "RMSE before IK : " << rmse << "\n";
+    
+    #endif
+
+    for (int iter=0; iter<4; ++iter) {
+      Eigen::VectorXd dq = skyweave::SolveIKStep(
+          *(this->pin_model_), 
+          *(this->pin_data_),
+          this->joint_positions_,
+          /* add constraints=*/this->constraints,
+          this->position_measurements,
+          this->frame_ids_);
+
+      // Eigen::VectorXd next_pos = Eigen::VectorXd::Zero(this->pin_model_->nq);
+      this->joint_positions_ = pinocchio::integrate(
+          *(this->pin_model_), this->joint_positions_, dq);
+ 
+      // this->joint_positions_ = next_pos;
+      
+      #ifdef CHECK_RMSE
+      // TODO check the link positions and compare them to the current model state
+      pinocchio::forwardKinematics(*(this->pin_model_), *(this->pin_data_), this->joint_positions_);
+      pinocchio::updateFramePlacements(*(this->pin_model_), *(this->pin_data_));
+
+
+      double sum_squared_error = 0.0;
+      std::size_t sample_count = 0;
+      for (const auto& [grid_index, measurement] : this->position_measurements) {
+        auto frame_it = this->frame_ids_.find(grid_index);
+        if (frame_it == this->frame_ids_.end()) {
+          continue;
+        }
+
+        const pinocchio::SE3& frame_placement = this->pin_data_->oMf[frame_it->second];
+        const Eigen::Vector3d current_position = frame_placement.translation();
+        this->current_positions_[grid_index] = current_position;
+
+        const Eigen::Vector3d diff = current_position - measurement;
+        sum_squared_error += diff.squaredNorm();
+        ++sample_count;
+      }
+
+      const double rmse = sample_count > 0
+                              ? std::sqrt(sum_squared_error / static_cast<double>(sample_count))
+                              : 0.0;
       if (rmse > prev_rmse) {
-        std::cerr << "RMSE increased from " << prev_rmse << " to " << rmse
+        std::cout << "RMSE increased from " << prev_rmse << " to " << rmse
                   << " after iteration " << iter << ".\n";
       }
       prev_rmse = rmse;
+
+      std::cout << "RMSE after iteration " << iter << ": " << rmse << "\n";
+
+      #endif
     
     }
     // Here you would implement the state estimation logic using
@@ -116,7 +163,15 @@ class StateEstimator {
       this->joint_velocities_.setZero();
       this->first_update_done = true;
     } else {
+      Eigen::VectorXd delta = Eigen::VectorXd::Zero(this->pin_model_->nv);
+      pinocchio::difference(
+          *(this->pin_model_),
+          prev_pos,
+          this->joint_positions_,
+          delta);
 
+      this->joint_velocities_ = delta / this->time_step_;
+      // std::cout << "Estimated mean joint velocities : " << this->joint_velocities_.transpose().mean() << "\n";
     }
 
   }
