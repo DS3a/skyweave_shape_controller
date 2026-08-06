5b9ce18c34e036bf72393bf19266a53f58cabeb7
DS3a <33351122+DS3a@users.noreply.github.com>
2026-08-06 19:28:39 +0200

    shit done

--- include/shape_controller_linearized.hpp
+++ include/shape_controller_linearized.hpp
@@ -26,5 +26,78 @@
 #include <vector>
 
 namespace skyweave {
-    using GridIndex = std::pair<int, int>;
+namespace controller {
+
+
+using namespace casadi;
+class ShapeControllerLinearized {
+    std::unique_ptr<skyweave_sim::Springs> springs_;
+    std::vector<skyweave::ConstraintPair> constraints_;
+    std::shared_ptr<skyweave::StateEstimator> state_estimator_;
+    std::shared_ptr<skyweave::controller::GammaSurface> gamma_surface_;
+    std::shared_ptr<pinocchio::Model> pin_model_;
+    pinocchio::Data pin_data_;
+    Eigen::VectorXd required_joint_positions_; // size nq
+    Eigen::VectorXd desired_joint_acceleration_;
+    Eigen::VectorXd spring_torques_;
+    Eigen::Quaterniond centre_orientation_;
+
+
+    // linearization variables
+    Eigen::VectorXd q_c;
+    Eigen::VectorXd v_c;
+    Eigen::VectorXd u_c;
+    
+    // linearizing the forward dynamics equality constraint
+    Eigen::MatrixXd Afx;
+    Eigen::MatrixXd Bfu;
+    Eigen::VectorXd c_fxu;
+
+    // linearizing the inequality constraints
+    Eigen::MatrixXd H;
+    Eigen::VectorXd H_c;
+
+    // linearizing the 0 gaussian curvature equality constraints
+    Eigen::MatrixXd G;
+    Eigen::VectorXd G_c;
+
+
+
+    Eigen::VectorXd previous_thrusts_;
+    casadi::DM A_dm_; // maping from thruster forces to joint torques, size nv x num_thrusters
+    casadi::DM thrusts_dm_; // size num_thrusters x 1
+    std::unique_ptr<skyweave::ConstrainedIKSolver> ik_solver_;
+
+    std::vector<skyweave::GridIndex> ordered_indices;
+    double kp_ = 10.0;
+    double kd_ = 2.0;
+    double ki_ = 0.01;
+    Eigen::VectorXd integral_error_; // size nv
+    int num_thrusters_=25;
+
+    // needs the current state (q, v) : which you can get from state estimator
+    // needs the setpoints from gamma surface
+    // calculate the required q from IK using gamma surface
+    ShapeControllerLinearized(std::shared_ptr<skyweave::StateEstimator> state_estimator,
+                    std::shared_ptr<skyweave::controller::GammaSurface> gamma_surface,
+                    std::shared_ptr<pinocchio::Model> pin_model);
+
+    void linearize_model(); // acquire the current state from state_estimator_
+    // use that to calculate A, B, c, H, H_c, G, G_c
+
+
+    void acquire_spring_model(std::unique_ptr<skyweave_sim::Springs> springs) {
+        this->springs_ = std::move(springs);
+
+    }
+    
+    void setSpringTorques(const Eigen::VectorXd& spring_torques);
+    void ComputeRequiredJointPosAndAccel();
+    std::map<skyweave::GridIndex, double> ComputeControlStep();
+
+};
+
+
+
+}    
 }
---
+++ src/shape_controller_linearized.cpp
@@ -0,0 +1,79 @@
+#include <skyweave_controller_linearized.hpp>
+
+namespace skyweave::controller {
+
+
+ShapeControllerLinearized::ShapeControllerLinearized(
+    std::shared_ptr<skyweave::StateEstimator> state_estimator,
+    std::shared_ptr<skyweave::controller::GammaSurface> gamma_surface,
+    std::shared_ptr<pinocchio::Model> pin_model)
+    : state_estimator_(std::move(state_estimator)),
+      gamma_surface_(std::move(gamma_surface)),
+      pin_model_(std::move(pin_model)),
+      pin_data_(*pin_model_) {
+  this->required_joint_positions_ =
+      pinocchio::neutral(*(this->pin_model_));  // neutral;
+  this->desired_joint_acceleration_ =
+      Eigen::VectorXd::Zero(this->pin_model_->nv);
+  this->integral_error_ = Eigen::VectorXd::Zero(this->pin_model_->nv);
+  this->spring_torques_ = Eigen::VectorXd::Zero(this->pin_model_->nv);
+  this->ik_solver_ = std::make_unique<skyweave::ConstrainedIKSolver>(
+      pin_model_, state_estimator_->FrameIds());
+
+  this->gamma_surface_->init_ik_solver(
+      pin_model_, state_estimator_->FrameIds());
+  this->constraints_ =
+      skyweave::BuildConstraintFrames(
+          this->gamma_surface_->num_elements_,
+          this->gamma_surface_->num_elements_);
+  this->previous_thrusts_ =
+      Eigen::VectorXd::Zero(static_cast<int>(state_estimator_->FrameIds().size()));
+  this->num_thrusters_ =
+      static_cast<int>(state_estimator_->FrameIds().size());
+
+  for (auto constraint : this->constraints_) {
+    std::cout << "Constraint between (" << constraint.first.first << ", "
+              << constraint.first.second << ") and ("
+              << constraint.second.first << ", " << constraint.second.second
+              << ")\n";
+  }
+
+  this->ordered_indices.reserve(state_estimator_->FrameIds().size());
+  for (const auto& [index, frame_id] : state_estimator_->FrameIds()) {
+    this->ordered_indices.push_back(index);
+  }
+}
+
+void linearize_model() {
+
+    int nv = this->pin_model_->nv;
+
+    this->q_c = this->state_estimator_->CurrentJointPositions();
+    this->v_c = this->state_estimator_->CurrentJointVelocities();
+    this->u_c = Eigen::VectorXd::Zero(nv); // TODO get the last output from the thingi. or get the open loop controllers input for the first time
+    Eigen::VectorXd tau = Eigen::VectorXd::Zero(nv);// TODO spring torques + fext passed through jacobians
+
+    Eigen::MatrixXd dq_dot_dq = Eigen::MatrixXd::Zero(nv, nv);
+    Eigen::MatrixXd dq_dot_dv = Eigen::Matrix::Identity(nv, nv);
+    // TODO check this. this isn't correct because q contains quaternions.
+    // q_dot = N(q)v
+    // if q_dot = v, then the derivative with respect to q and v would have been 0 and I respectively
+    // but now they are v dN(q)/dq and N(q) respectively
+
+    Eigen::MatrixXd ddv_dq = Eigen::MatrixXd(nv, nv);
+    Eigen::MatrixXd ddv_dv = Eigen::MatrixXd(nv, nv);
+    pinocchio::computeABADerivatives(this->pin_model_, this->pin_data_, q_c, q_v, tau);
+    ddv_dq = this->pin_data_->ddq_dq;
+    ddv_dv = this->pin_data_->ddq_dv;
+
+    this->Afx = Eigen::MatrixXd(2*nv, 2*nv);
+    this->Afx.topLeftCorner(nv, nv) = dq_dot_dq;
+    this->Afx.topRightCorner(nv, nv) = dq_dot_dv;
+    this->Afx.bottomLeftCorner(nv, nv) = ddv_dq;
+    this->Afx.bottomRightCorner(nv, nv) = ddv_dv;
+
+
+}
+
+
+}
